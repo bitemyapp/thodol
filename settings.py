0cce4ed6c3539ef3c98c165061fc88d4fb193eb3
Chris Allen <cma@bitemyapp.com>
2011-02-04 06:30:37 -0500

    apps, models

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'thodol',                      # Or path to database file if using sqlite3.
+        'NAME': 'thodol.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -91,5 +91,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'wiki',
 )
--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    name = models.CharField(max_length=200)
+
+class Discussion(models.Model):
+    tldr = models.TextField()
+
+class Code(models.Model):
+    discussion = models.ForeignKey(Discussion)
+    code = models.TextField()
